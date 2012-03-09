2bd8dbddf5c90d79f165b96851a9fa11a3f5d755
Simon Lüke <semaphor@resolution1.net>
2012-03-09 20:52:04 +0100

    Solved sending reminder mails; thus closing issue #1; to be reviewed.

--- mailit.py
+++ mailit.py
@@ -6,6 +6,7 @@
 # powering for uuulm and StuVe:
 # simon.fuchs@uni-ulm.de, simon.lueke@uni-ulm.de
 
+# Written and tested for Python 2.7.2+ on Ubuntu 11.10
 
 """
 
@@ -20,30 +21,32 @@ Skizze Vorgehen
 
  * StuVe erstellt Umfrage in Evasys.
  * StuVe holt OK für die Verschickung von Zentraler Evaluation und Rechtsabteilung ein.
- * StuVe übergibt kiz Mailtext, Skript, ... und Liste mit TANs.
- * kiz generiert Liste mit Mailadressen aller Studierender.
- * kiz versendet die Mails (mittels Skript, Mailtext und beiden Listen): mailit.py
- * kiz löscht die Liste mit Mailadressen nicht!
+ * StuVe übergibt kiz Mailtext (mail.txt), Skript (mailit.py), und Liste mit TANs
+   (tanlist.txt).
+ * kiz generiert Liste mit Mailadressen aller Studierender
+   (addresslist.txt).
+ * kiz versendet die Mails (mittels Skript, Mailtext und beiden Listen)
+   (python mailit.py erstversand).
+ * kiz behält die Liste mit den Mailadressen!
 
 Abwarten, c.a. 2 Wochen.
 
 2. Verschickung (Erinnerung):
 
- * StuVe übergibt kiz eine neue Liste mit noch nicht genutzten TANs und ein weiteres Skript zum abgleich.
- * kiz gleicht alte und neue TAN-Liste mit der Liste der Mailadressen ab, Adressen deren TAN schon verwendet werden gelöscht: reminderlist.py
- * kiz versendet mit abgeglichener Liste von Mailadressen und neuer TAN-Liste eine Erinnerung.
- * kiz löscht nach erfolgreicher 2. Verschickung alle angefallenen Dateien.
+ * StuVe übergibt kiz eine neue Liste mit noch nicht genutzten TANs und einen angepassten Mailtext
+   (tanslist-reminder.txt, mail-reminder.txt).
+ * kiz versendet Erinnerungen, das Skript versendet dazu durch Abgleich beiden TAN-Listen nur noch an Personen, die noch nicht teilgenommen haben
+   (python mailit.py erinnerung).
+ * kiz löscht nach erfolgreicher 2. Verschickung alle verwendeten Dateien.
 
 
 Informationen
 -------------
 
  * Anzahl TANs (zeilenweise) in der TAN-Liste muss >= Anzahl der Emailadressen in der Adressliste (ebenfalls zeilenweise).
- * Dateien addresslist.txt und tanlist.txt am besten ohne Leerzeichen und Leerzeilen am Ende, sons wird mit der letzten Zeile noch ein Fehler erzeugt.
-
-
-
-Written and tested with Python 2.7.2+ on Ubuntu 11.10
+ * Reihenfolge der TANs in den beiden Listen muss gleich sein, die Listen liefert EvaSys bereits in dieser Form.
+ * Dateien addresslist.txt, tanlist.txt und tanlist-reminder.txt am besten ohne Leerzeichen und Leerzeilen am Ende.
+   Es kommst sonst evtl. zur Ausgabe von unnötigen Fehlermeldungen, Versand sollte aber trotzdem klappen.
 
 
 Dateiformate
@@ -54,10 +57,12 @@ file-5.04
 magic file from /etc/magic:/usr/share/misc/magic
 
 $ file *
-addresslist.txt:     ASCII text
-mailit.py:           a /usr/bin/python script text executable
-mail.txt:            UTF-8 Unicode text
-tanlist.txt:         ASCII text
+addresslist.txt:                ASCII text
+mailit.py:                      a /usr/bin/python script text executable
+mail.txt:                       UTF-8 Unicode text
+mail-reminder.txt:              UTF-8 Unicode text
+tanlist.txt:                    ASCII text
+tanlist-reminder.txt:           ASCII text
 
 
 """
@@ -65,33 +70,71 @@ tanlist.txt:         ASCII text
 
 # Konfiguration
 
-# Liste der TANs, zeilenweise
-tanFile = 'tanlist.txt'
-# Liste der Mailempfaenger, zeilenweise
+# Liste aller Mailempfaenger, zeilenweise
 addressFile = 'addresslist.txt'
-# Nachricht die gesendet werden soll, $TAN$ wird darin durch die TAN ersetzt
+# Liste der TANs, zeilenweise; -reminder.txt für Erinnerungsmail, gleiche Reihenfolge der verbliebenen TANs
+tanFile = 'tanlist.txt'
+tanFileReminder = 'tanlist-reminder.txt'
+# Nachricht die gesendet werden soll, $TAN$ wird darin durch die TAN ersetzt; ebenfassl -reminder.txt
 mailFile = 'mail.txt'
+mailFileReminder = 'mail-reminder.txt'
 # Encoding für die Mails (Möglichkeiten: 'iso-8859-1', 'iso-8859-15', 'utf-8')
 mailEncoding = 'iso-8859-15'
+# Erstversand oder Erinnerung, erstes und einziges Kommandozeilenargumen; Default: Erstversand <-> False
+reminder = False
+# Sekunden um evtl. Abbruch durch Nutzer vor dem Versand abzuwarten
+sleepTime = 1
 
+# Bibliotheken
 
-# los gehts
 import smtplib
 from email.message import Message                                       # eigentliche Klasse für Emails
 from email.header import Header                                         # um Umlaute im Header richtig kodieren zu können
 from email.parser import Parser                                         # um aus einer Datei die Email "parsen" zu können
+import sys                                                              # für Kommandozeilenargumente
+import time                                                             # Wartezeit
 
 
-# TANs aus tanFile lesen
+# los gehts
+
+
+# Kommandozeilenargument überprüfen
+try:
+    if ((len(sys.argv) != 2) or (sys.argv[1] not in ['erstversand', 'erinnerung'])):
+        raise NameError('Arguemntfehler')
+except:
+    print '\n  Usage:   ' + sys.argv[0] + ' <versandart>\n\n     <versandart> = erstversand | erinnerung\n\n'
+    sys.exit(1)
+else:
+    if sys.argv[1] == 'erinnerung':
+        reminder = True
+        print '\n(i) Beginne mit Versand von ERINNERUNGEN in ' + str(sleepTime) + ' Sekunden. CTRL-C um abzubrechen.\n\n'
+        time.sleep(sleepTime)
+    else:
+        print '\n(i) Beginne mit Versand an GESAMTE LISTE in ' + str(sleepTime) + ' Sekunden. CTRL-C um abzubrechen.\n\n'
+        time.sleep(sleepTime)
+
+
+# TANs aus Dateien lesen
 tans = []
-print ("(i) reading tans from " + tanFile + ".")
+tansReminder = []
+print '(i) reading tans from ' + tanFile + '.'
 for line in open(tanFile, 'r'):
     tans.append(line)
-print ("(i) got " + str(len(tans)) + " tans.\n")
+print '(i) got ' + str(len(tans)) + ' tans.\n'
+if reminder:
+    print '(i) reading remaining tans from ' + tanFileReminder + '.'
+    for line in open(tanFileReminder, 'r'):
+        tansReminder.append(line)
+    print '(i) got ' + str(len(tansReminder)) + ' tans.\n'
 
 
 # Email aus mailFile lesen, Zeichensätze auf ISO 8859-1 setzen
-mail = Parser().parse(open(mailFile, 'rb'))
+if not reminder:
+    mail = Parser().parse(open(mailFile, 'rb'))
+else:
+    mail = Parser().parse(open(mailFileReminder, 'rb'))
+
 
 myHeader = mail['Subject']
 del mail['Subject']
@@ -101,53 +144,78 @@ assert(mail.is_multipart() == False)
 unicodeBody = unicode(mail.get_payload(None, True), 'utf-8')
 mail.set_payload(unicodeBody.encode(mailEncoding), mailEncoding)
 
-print("(i) summarized header of mail:\n")
-print (mail.items())
-print("")
+print '(i) summarized header of mail:'
+print mail.items()
+print
 
 
 # Mailer initialisieren
-print ("(i) loading mailer...\n")
+print '(i) loading mailer...\n'
 s = smtplib.SMTP('smtp.uni-ulm.de')
 
 
 # Mails an alle Adressen aus addressFile senden
-print ("(i) sending mails, reading recipients from " + addressFile + ".\n")
-# counter fuer die tan-nummern und erste TAN einfügen
-tannr = 0
-mail.set_payload(mail.get_payload().replace('$TAN$',tans[tannr]))
+print '(i) sending mails, reading recipients from ' + addressFile + '.\n'
+
+# counter fuer die TAN-Nummern
+tanNr = 0
+tanNrReminder = 0
+tansReminderLength = len(tansReminder)
+mailCount = 0
+
+# Erste Mail muss $TAN$ ersetzten, dazu
+tanNrLastSent = -1
+tans.append('$TAN$')
 
 # Abarbeiten
 for line in open(addressFile, "r"):
 
+    if reminder:
+        if tanNrReminder >= tansReminderLength:
+            print '\n\n(i) sent mails for all entries in ' + tanFileReminder
+            break
+        print '(i) processing TAN ' + tans[tanNr].rstrip() + ', looking for ' + tansReminder[tanNrReminder].rstrip()
+        if tans[tanNr] != tansReminder[tanNrReminder]:
+            tanNr += 1
+            continue
+
     # To-Feld in Mailheader einfügen
-        mail.__delitem__('To')
-        mail['To'] = line.rstrip()
-        try:
-                print "(i) sending mail to: " + mail['To']                                        #.items()
-                s.sendmail(mail['From'], line, mail.as_string())
+    mail.__delitem__('To')
+    mail['To'] = line.rstrip()
+    try:
+        print '(i) preparing and sending mail to: ' + mail['To'] + ' with TAN ' + tans[tanNr].rstrip()
+        mail.set_payload(mail.get_payload().replace(tans[tanNrLastSent], tans[tanNr]))
+        tanNrLastSent = tanNr
+        s.sendmail(mail['From'], line, mail.as_string())
 
     # Fehlerbehandlung
-        except smtplib.SMTPRecipientsRefused:
-                print ("ERROR on sending mail to:" + line.rstrip())
-                break
+    except smtplib.SMTPRecipientsRefused:
+        print '\nERROR on sending mail to:' + line.rstrip() +'\n'
+        sys.exit(2)
 
-        # nächste TAN einfuegen
-        tannr += 1
-        mail.set_payload(mail.get_payload().replace(tans[tannr-1], tans[tannr]))
+    mailCount += 1
+    print '(i)     ... sent! (line ' + str(tanNr+1) + ' in ' + addressFile + ')'
 
-        print ("(i) sent mail number "+str(tannr))
+    # damit wurde eine Mail versendet, also Zähler erhöhen
+    tanNr += 1
+    tanNrReminder += 1
 
 # Ende
+
+print '\n\n(i) Assuming to be done, please check if numbers are plausible.\n\n    Number of sent mails: ' + str(mailCount)
+
+
 s.quit()
-print ("\n done.\n")
+
+
+print '\n\n done.\n'
 
 
 
 """
 
-Für spätere Versionen
-=====================
+Für Weiterentwicklungen
+=======================
 
  *Encoding?
   * Quelldatei für Mails gleich in der Zielkodierung?
@@ -157,5 +225,7 @@ Für spätere Versionen
   * Welches Encodung ist im From-Feld erlaubt? TB erzeugt bei Simon L. beim Uni-IMAP eine merkwürdige Darstellung.
 
  * Ersetzen der TANs könnte schöner gelöst werden.
+
+ * newline am Ende von tan[] entfernen
 
  """
